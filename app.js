6080e328f7432a3cce6b7baef9d5b49035249af2
Sean M <sean@arrowburn.co.nz>
2018-05-23 18:01:46 +0200

    testing driver controller POST

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
+const mongoose = require('mongoose')
 const routes = require('./routes/routes')
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/muber')
 
 app.use(bodyParser.json());
 routes(app);
