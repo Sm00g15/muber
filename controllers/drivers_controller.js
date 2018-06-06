2f7e6f0e817e926cb3492e0d60c1c48371b83161
Sean M <sean@arrowburn.co.nz>
2018-06-06 14:10:34 +0200

    grammatical errors

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -4,7 +4,7 @@ module.exports = {
 	greeting(req, res) {
 		res.send({hi: 'there '})
 	},
-	create(req, res, next) {
+	create(req, res, next ) {
 		const driverProps = req.body;
 		Driver.create(driverProps)
 			.then(driver => res.send(driver))
